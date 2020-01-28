62a22fca36dc851b1c0c115c3fd5562d14b5fe73
mrkai1253 <asishshajithomas@gmail.com>
2020-01-28 23:26:04 +0530

    Changed project ui #1

---
+++ components/ProjectCard.js
@@ -0,0 +1,82 @@
+import {
+    Dimensions,
+    Image,
+    ImageBackground,
+    StyleSheet,
+    Text,
+    View
+} from "react-native";
+
+import { Entypo } from '@expo/vector-icons';
+import React from "react";
+
+const { width, height } = Dimensions.get('window')
+
+
+const componentName = (props) => (
+    <ImageBackground style={styles.imageBG}
+        imageStyle={{
+            borderRadius: 20
+        }}
+        blurRadius={100000000}
+        resizeMode="cover"
+        blurRadius={20}
+        source={{
+            uri:
+                require('../assets/images/bg.jpg')
+        }}>
+        <View style={{ flex: 1, }}>
+            <Image
+                style={styles.projectThumb}
+                source={{
+                    uri:
+                        "https://lh3.googleusercontent.com/-igmIShQcgSH0wf3TZUtGjg20oG5-ha9Mm6i5m6tGa48t5WyUwiNdIUb-NfD4G9DTg=w1920-h977-rw"
+                }} />
+        </View>
+        <View style={styles.titleContainer}>
+            <Text style={styles.title}>DYUSKSHA 20</Text>
+            <Text style={styles.details} numberOfLines={4} ellipsizeMode="clip">Dyuksha 20 is a remarkable vision propelled by an incredible reason and brought about by a strong spark which makes one's contemplations break bonds to sustain a place where nothing and literally nothing is unimaginable. The national level techno-administrative social symposium is an amalgamation of science and artistry. It is innovation and technology acting together, catalyzed by inventiveness, to make the panacea. The intention of Dyuksha 20 is to compose a definitive outlay of innovation, business, culture and flar.</Text>
+
+            <View style={{ flexDirection: 'row', padding: 5, justifyContent: 'space-between' }}>
+                <Entypo name="google-play" size={32} color="white" onPress={() => Linking.openURL('https://github.com/asishshaji/')} />
+                <Entypo name="github-with-circle" size={32} color="white" onPress={() => Linking.openURL('https://github.com/asishshaji/')} />
+            </View>
+
+        </View>
+
+
+
+    </ImageBackground>
+)
+export default componentName;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    title: {
+        fontFamily: 'medium',
+        fontSize: 26,
+        color: 'white',
+        textAlign: 'center',
+        padding: 5
+    },
+    titleContainer: {
+        justifyContent: 'center', alignItems: 'center', flex: 2, padding: 10,minWidth:100
+    },
+    projectThumb: { height: 250, width: 200, left: 25, right: 25, borderRadius: 20, top: -25, bottom: -25 }
+    ,
+    imageBG: {
+        height: 200, width: 700,
+        backgroundColor: '#000',
+        borderRadius: 20, position: 'relative', margin: 50, flexDirection: 'row',marginTop:20
+    },
+    details: {
+        fontFamily: 'medium',
+        fontSize: 18,
+        color: '#fff',
+        maxHeight: 200
+    }
+});
--- screens/Projects.js
+++ screens/Projects.js
@@ -9,6 +9,9 @@ import {
 } from "react-native";
 import React, { Component } from "react";
 
+import Card from '../components/ProjectCard'
+import { Ionicons } from '@expo/vector-icons';
+
 class Projects extends Component {
 
     static navigationOptions = {
@@ -16,12 +19,13 @@ class Projects extends Component {
     }
     componentDidMount() {
         Font.loadAsync({
-            'bold': require('../assets/fonts/ProductSans-Bold.ttf'),
+            'bold': require('../assets/fonts/ProductSans-Black.ttf'),
             'medium': require('../assets/fonts/ProductSans-Medium.ttf')
         });
     }
 
     render() {
+        const navigate = this.props.navigation
         return (
             <ImageBackground
                 style={{ flex: 1, backgroundColor: '#000' }}
@@ -30,44 +34,21 @@ class Projects extends Component {
                 source={{
                     uri:
                         require('../assets/images/bg.jpg')
-                }}>
+                }}
+
+            >
 
                 <View style={styles.container}>
                     <View style={styles.titleContainer}>
+                        <Ionicons name="ios-arrow-back" size={32} color="white" onPress={() => navigate.goBack()} style={{ position: 'absolute', bottom: 10, left: 20, zIndex: 100000000 }} />
                         <Text style={styles.projectHeader}>PROJECTS</Text>
                     </View>
-                    <View style={{ marginTop: 130, flex: 1 }}>
+                    <View style={{ marginTop: 130, flex: 1, alignSelf: 'center', padding: 20 }}>
 
-                        <View style={{ borderRadius: 20, marginLeft: 10 }}>
-                            <ImageBackground style={{
-                                height: 300, width: 300,
-                                backgroundColor: '#000',
-                                borderRadius: 20, position: 'relative'
-                            }}
-                                imageStyle={{
-                                    borderRadius: 20
-                                }}
-                                blurRadius={20}
-                                resizeMode="cover"
-                                // blurRadius={20}
-                                source={{
-                                    uri:
-                                        require('../assets/images/profile.jpg')
-                                }}>
-                                <Image
-                                    style={{ height: 250, width: 250, position: 'absolute', top: -100, left: 25, right: 25, borderRadius: 20 }}
-                                    source={{
-                                        uri:
-                                            require('../assets/images/bg.jpg')
-                                    }} />
+                        <Card />
+                        <Card />
 
-                                <Text style={{
-                                    fontFamily: 'medium', fontSize: 26,
-                                    color: 'white', position: 'absolute', bottom: 20, textAlign: 'center', left:10,right:10
-                                }}>DYUSKSHA 20 ANDROID APP</Text>
 
-                            </ImageBackground>
-                        </View>
 
                     </View>
 
@@ -81,15 +62,17 @@ export default Projects;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+
     },
     projectHeader: {
         fontFamily: 'bold',
         color: 'white',
         fontSize: 40,
-        textAlign: 'center'
+        textAlign: 'center',
     },
     titleContainer: {
-        marginTop: 30
+        marginTop: 30,
+        position: 'relative'
     }
 
 });
